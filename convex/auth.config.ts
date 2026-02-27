79618fd9530fa5bb9a38470c9a1f76f1b50d3cf1
Nasir@415998 <noorbashanasiruddin.com>
2026-02-27 15:23:53 +0530

    fix: resolve a deployment issue

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://gentle-chimp-66.clerk.accounts.dev", 
+      domain: process.env.CLERK_ISSUER_URL || "https://gentle-chimp-66.clerk.accounts.dev", 
       applicationID: "convex",
     },
   ],
